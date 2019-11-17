99b3d37219221c7faee72f5059468ef895e5345c
4d11 <aadilasim@hotmail.com>
2019-11-16 17:06:44 -0800

    Updated UI

--- mobile/LoginView.js
+++ mobile/LoginView.js
@@ -1,51 +1,67 @@
-import React, { Component } from 'react';
-import { View, Button, Text, Image } from 'react-native';
+import React, {Component} from 'react';
+import {View, Button, Text, Image} from 'react-native';
 import * as Facebook from 'expo-facebook';
 import axios from 'axios';
 import EventListView from './EventListView';
 
 export default class LoginView extends Component {
-  state = { };
+    state = {};
 
-  login = async () => {
-    try {
-      const {
-        type,
-        token,
-        expires,
-        permissions,
-        declinedPermissions,
-      } = await Facebook.logInWithReadPermissionsAsync('981730282184579', {
-        permissions: ['public_profile', 'email', 'user_hometown', 'user_likes', 'user_link'],
-      });
-      if (type === 'success') {
-        const response = await axios(`https://graph.facebook.com/me?access_token=${token}`);
-        alert(`Hi ${response.data.name}, welcome to Facebook IRL!`);
-        this.setState({ login: response.data });
-      } else {
-        alert('Login cancelled :(')
-      }
-    } catch ({ message }) {
-      alert(`Facebook Login Error: ${message}`);
+    login = async () => {
+        try {
+            const {
+                type,
+                token,
+                expires,
+                permissions,
+                declinedPermissions,
+            } = await Facebook.logInWithReadPermissionsAsync('981730282184579', {
+                permissions: ['public_profile', 'email', 'user_hometown', 'user_likes', 'user_link'],
+            });
+            if (type === 'success') {
+                const response = await axios(`https://graph.facebook.com/me?access_token=${token}`);
+                alert(`Hi ${response.data.name}, welcome to Facebook IRL!`);
+                this.setState({login: response.data});
+            } else {
+                alert('Login cancelled :(')
+            }
+        } catch ({message}) {
+            alert(`Facebook Login Error: ${message}`);
+        }
     }
-  }
 
-  render() {
-    return (
-      this.state.login ?
-        <EventListView /> :
-        <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          <Image source={require('./assets/logo.png')} style={{width: 200, height: 200, marginBottom: 100}} />
-          <Text style={{ fontSize: 40 }}>Login to Facebook</Text>
-          <Text style={{ fontSize: 20, marginBottom: 20 }}>to access premiere events near you!</Text>
-          <Button style={{ borderRadius: 40 }} onPress={async () => await this.login()} title="Login to Facebook!" />
-        </View>
-    );
-  }
+    render() {
+        return (
+            this.state.login ?
+                <EventListView/> :
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#3b5998'
+                }}>
+                    <Image source={require('./assets/logo.png')} style={{width: 200, height: 200, marginBottom: 100}}/>
+                    <Text style={{fontSize: 18, marginBottom: 20, color: 'white', fontfamily: 'Roboto'}} >
+                        The AR social network for real connections.
+                    </Text>
+                    <Button
+                        style={{
+                            borderWidth: 1,
+                            borderColor: 'rgba(0,0,0,0.2)',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: 150,
+                            height: 150,
+                            borderRadius: 150,
+                            fontFamily: 'Roboto'
+                        }}
+                        color={'#f7f7f7'}
+                        title={'Continue with Facebook'}
+                        onPress={async () => await this.login()}
+                    />
+                </View>
+        );
+    }
 };
 
 module.exports = LoginView;
